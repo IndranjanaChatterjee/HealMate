7fd02632479e919028d4bef9b59323aacc5c1a6b
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-03-03 11:19:45 +0530

    bottom

---
+++ frontend/src/Components/Home_about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Home_about() {
+  return (
+    <div>
+        <div className="box">
+      <p className='text-[#ffffff]'>
+      Say goodbye to the uncertainty of self-diagnosis and endless internet searches. Our revolutionary platform utilizes advanced AI algorithms to accurately diagnose health conditions based on your symptoms or even a simple picture.
+      </p>
+      <p className="join">
+        
+      </p>
+      </div>
+    </div>
+  )
+}
