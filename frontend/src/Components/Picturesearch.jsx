5b8eba823a11bcc712855bc99488c39cb6f1f895
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-03-03 11:14:45 +0530

    Routes

---
+++ frontend/src/Components/Picturesearch.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Bottomnav from './Bottomnav'
+
+export default function Picturesearch() {
+  return (
+    <div>
+      picture
+      <div className="down fixed bottom-[3rem] w-[100%] flex justify-center items-center">
+            <Bottomnav/>
+        </div>
+        
+    </div>
+  )
+}
