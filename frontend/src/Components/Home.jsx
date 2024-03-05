90420bc5581594076fc888ebc11a7d634f2e45b7
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-03-05 22:28:20 +0530

    add

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -3,6 +3,7 @@ import Videosection from './Videosection';
 import Bottomnav from './Bottomnav';
 import Topnav from './Topnav';
 import Home_about from './Home_about';
+import Footer from './Footer';
 
 
 export default function Home() {
@@ -14,8 +15,11 @@ export default function Home() {
         
         <Videosection/>
         <div className='homebtn min-h-[110vh]'>
+            <div className="grady h-[10vh]"></div>
             <Home_about/>
+            <Footer/>
         </div>
+        
         <div className="down fixed bottom-[1rem] w-[100%] flex justify-center items-center">
             <Bottomnav/>
         </div>
