09f740a7e939669999d04ef045370c4bbc4f2759
Ritwika Mahapatra <119119223+Ritwika-14@users.noreply.github.com>
2024-03-09 15:21:57 +0530

    Update Error.jsx

--- frontend/src/Components/Error.jsx
+++ frontend/src/Components/Error.jsx
@@ -3,26 +3,21 @@ import { NavLink } from 'react-router-dom';
 
 const Error = () => {
   return (
-    <div className="flex justify-center items-center m-0 p-0 outline-none box-border font-sans h-screen bg-gradient-to-br from-sap2 to-dark bg-fixed bg-cover bg-repeat">
-      <div className=" h-3/4 w-3/4 flex items-center justify-center bg-dark shadow-4xl rounded-xl">
-        <div className="max-w-2xl mx-auto text-center">
+    <div className="error w-screen h-screen justify-center items-center text-center">
+          <div className=" mx-auto py-[150px] text-center">
           <h2 className="mb-5 text-9xl leading-none relative">
-            <span className="text-sap2 bg-clip-text">404</span>
+            <span className="text-[#ffffff] text-center glow m-[2rem] text-[6rem] md:w-[50%] w-[90%]">ERROR</span>
           </h2>
-          <h4 className="text-2xl mb-6 uppercase relative">
-            <span className="text-sap bg-clip-text text-transparent">
-              Opps! Page not found
-            </span>
-          </h4>
-          <p className="max-w-2xl mx-auto text-center text-lg text-sap">
-            Sorry, the page you're looking for doesn't exist. If you think something is broken, report a problem.
-          </p>
-          <div className="max-w-2xl mx-auto text-center mt-6 flex-row inline-block py-2 px-6 border-2 border-sap2 text-light font-semibold rounded-full uppercase transition duration-300 ease-in-out hover:bg-sap2 hover:text-dark">
-            <NavLink to='/'>Return to Home Page.</NavLink>
-          </div>
-        </div>
+            <p className="text-[#ffffff] glow m-[2rem] text-[2.5rem]">
+            An unknown error has occurred. Go back to home page</p>
+
+          <div className="text-[2rem] w-4xl mx-auto text-center mt-9 inline-block py-2 px-6 btn rounded-lg
+           text-light font-semibold transition duration-300 ease-in-out hover:bg-sap2 hover:text-dark
+            shadow-2xl" style={{ boxShadow: '0px 8px 8px -4px rgba(0, 0, 0, 0.2), 0px 0px 4px 0px rgba(0, 0, 0, 0.2)' }}>
+  <NavLink to='/'>Go Back</NavLink>
+</div>
+</div>
       </div>
-    </div>
   );
 };
 
